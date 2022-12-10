2e368671fd6a4d5d47d23f7ac4beb30682e7720c
Yuyang Lee <i@aidenli.net>
2022-12-10 17:21:01 +0800

    baseline

--- data/Faces.py
+++ data/Faces.py
@@ -6,8 +6,10 @@ LastEditTime: 2022-11-11 15:55:17
 Description: Dataset for face images.
 '''
 
+from collections import defaultdict
 import json
 import os
+import random
 import uuid
 from random import shuffle
 
@@ -20,7 +22,7 @@ from tqdm import tqdm
 import cv2
 
 class Faces(Dataset):
-    def __init__(self, base_dir, batch_size=64, H=128, W=128, mode='train', lazy=False, preload_device='cpu', device='cuda'):
+    def __init__(self, base_dir, batch_size=16, H=64, W=64, mode='train', train_ratio=0.8, lazy=False, preload_device='cpu', device='cuda'):
         super().__init__()
         
         self.lazy = lazy
@@ -29,100 +31,114 @@ class Faces(Dataset):
         self.batch_size = batch_size
         
         self.H, self.W = H, W
+        self.resizer = Resize((H, W))   # Bilinear
         
         self.preload_device = preload_device
         self.device = device
         
         if mode == 'train':
-            self.__getitem__ = self.train_getitem
-            self.__len__ = lambda: len(self.all_data)
+            self._getitem = self.train_getitem
+            self.ratio = train_ratio
         elif mode == 'valid':
-            self.__getitem__ = self.train_getitem
-            self.__len__ = lambda: len(self.all_data)
+            self._getitem = self.train_getitem
+            self.ratio = 1 - train_ratio
+        elif mode == 'test':
+            self._getitem = self.test_getitem
         else:
-            self.__getitem__ = self.test_getitem
-            self.__len__ = lambda: len(self.idx_to_name)
-        self._load(base_dir)
+            raise NotImplementedError("Mode not implemented.")
+        self._load(base_dir, mode)
         
-    # @torch.no_grad
-    def train_getitem(self, index):
-        image_0 = self.get_image(index).clone().to(self.devicee)
-        triplet, labels = self.get_triplet(index)
-        triplet_images = [ self.get_image(j).clone().to(self.devicee) for j in triplet ]
-        triplet_images = torch.stack(triplet_images, dim=0)
-        
-        return image_0, triplet_images, labels
+    def __len__(self):
+        return self.len
     
-    # @torch.no_grad
+    def __getitem__(self, index):
+        return self._getitem(index)
+        
+    def train_getitem(self, index):
+        img_anc = self.get_image(index).clone().to(self.device)
+        pos_idx, neg_idx = self.get_triplet(index)
+        pos_img = self.get_image(pos_idx).clone().to(self.device)
+        neg_img = self.get_image(neg_idx).clone().to(self.device)
+        return img_anc, pos_img, neg_img, torch.tensor(index).to(self.device), torch.tensor(pos_idx).to(self.device), torch.tensor(neg_idx).to(self.device)
+    
     def test_getitem(self, index):
-        image_0 = self.get_images(2 * index    ).clone().to(self.devicee)
-        image_1 = self.get_images(2 * index + 1).clone().to(self.devicee)
+        image_0 = self.get_images(2 * index    ).clone().to(self.device)
+        image_1 = self.get_images(2 * index + 1).clone().to(self.device)
         return image_0, image_1
     
     def get_triplet(self, index):
-        # TODO: Get amount of positive and negative pairs: n_p = min(B, N), n_n = B - n_p
-        # TODO: Sample positive pairs.
-        # TODO: Chooses collided pair and random pairs to flip
-        # TODO: Return index and labels
-        pass
+        name = self.idx_to_name[index]
+        person_idxs = self.name_to_idx[name]
+        pos_idx = random.choice(person_idxs)
+        neg_idx = random.choice(np.arange(0, person_idxs[0]).tolist() + np.arange(person_idxs[-1] + 1, self.len).tolist())
+        return pos_idx, neg_idx
+            
+    def _crop_and_resize(self, img, rect_x1, rect_y1, rect_x2, rect_y2):
+        img = img[rect_y1:rect_y2, rect_x1:rect_x2]
+        img = self.resizer(img.permute(2, 0, 1)).permute(1, 2, 0)
+        return img
     
     def train_get_image_pre(self, index):
-        i_name, i_file = self.all_data[index]
-        return self.aligned_images[i_name][i_file]
+        return self.aligned_images[index].to(self.device)
     
-    def train_get_image_lazy(self, index):
-        i_name, i_file = self.all_data[index]
-        path = self.aligned_images_paths[i_name][i_file]
-        img = torch.tensor(cv2.imread(path, cv2.IMREAD_COLOR)).float().to(self.preload_device)
-        rect_x1, rect_y1, rect_x2, rect_y2 = self.aligned_image_rect[i_name][1], self.aligned_image_rect[i_name][3], self.aligned_image_rect[i_name][0], self.aligned_image_rect[i_name][2]
-        # TODO: Pad the image for indicing
-        # TODO: Compute the expanded rectangle
-        # TODO: Crop with the expanded rectangle
-        # TODO: Resize image to H x W
+    def train_get_image_lazy(self, index, device='cuda'):
+        path = self.aligned_images_paths[index]
+        img = torch.tensor(cv2.imread(path, cv2.IMREAD_COLOR)).float().to(device)
+        rect_x1, rect_y1, rect_x2, rect_y2 = self.aligned_image_rect[index][1], self.aligned_image_rect[index][0], self.aligned_image_rect[index][3], self.aligned_image_rect[index][2]
+        img = self._crop_and_resize(img, rect_x1, rect_y1, rect_x2, rect_y2)
         return img
 
-    # @torch.no_grad
     def _load(self, base_dir, mode='train'):
         if mode == 'train' or mode == 'valid':
-            names = json.load(open(os.path.join(base_dir, "split.json"), 'r'))[mode]
             self.base_dir = os.path.join(base_dir, "training_set")
             
             self.idx_to_name = []
-            self.name_to_idx = {}
+            self.name_to_idx = defaultdict(list)
             
             self.aligned_images_paths = []
             self.aligned_image_rect = []
             
             self.aligned_images = []
             
-            self.all_data = []
-            
         elif mode == 'test':
             self.base_dir = os.path.join(base_dir, "test_pair")
-            names = os.listdir(os.path.join(self.base_dir, "test_pair"))
+        
+        meta = json.load(open(os.path.join(self.base_dir, "bb.json"), 'r'))
+        names = list(meta.keys())
+        random.shuffle(names)
+        names = names[:int(len(names) * self.ratio)]
+        
+        if 'failure' in names:
+            names.remove('failure')
+        
+        meta = { k: v for k, v in meta.items() if k in names }
             
-        tqdm.write("Loading data...")
-        for i_name, (name, metadata) in tqdm(enumerate(names.items())):
+        print(f"Loading data in { mode } mode...")
+        for i_name, (name, metadata) in enumerate(meta.items()):
             aligned_img_files = metadata['pics_aligned']
             
             # Prepare lists for data
-            self.aligned_images_paths.append([])
-            self.aligned_image_rect.append([])
-            self.aligned_images.append([])
-            
             for i_file, aligned_img_file in enumerate(aligned_img_files):
-                self.all_data.append((i_name, i_file))
-                
                 rectangle = metadata['align_params'][aligned_img_file]['rect_aligned']
+                
+                rectangle = torch.tensor(rectangle, device=self.device)
+                if (rectangle < 0).sum() > 0:
+                    print(f"\tSkipped { aligned_img_file } because of error values in rectangle.")
+                    continue
+                
                 aligned_file_path = os.path.join(self.base_dir, name, "aligned", aligned_img_file)
                 
-                self.name_to_idx[name] = len(self.idx_to_name)
+                self.name_to_idx[name].append(len(self.idx_to_name))
                 self.idx_to_name.append(name)
-                self.aligned_images_paths[-1].append(aligned_file_path)
-                self.aligned_image_rect.append(torch.array(rectangle))
+                self.aligned_images_paths.append(aligned_file_path)
+                self.aligned_image_rect.append(rectangle)
                 
-                if self.lazy:
-                    self.aligned_images[-1].append(self.train_get_image_lazy(len(self.all_data) - 1))
-                
-                self.get_image = self.train_get_image_lazy if self.lazy else self.train_get_image_pre
-            
+                if not self.lazy:
+                    self.aligned_images.append(self.train_get_image_lazy(len(self.idx_to_name) - 1, self.preload_device))
+                    self.get_image = self.train_get_image_pre
+                else:
+                    self.get_image = lambda i: self.train_get_image_lazy(i, device=self.device)
+                    
+        self.len = len(self.idx_to_name) / 2 if mode == 'test' else len(self.idx_to_name)
+        tqdm.write(f"Loading { self.len} (instances / pairs of) data in { mode } mode...")
+        
--- run.py
+++ run.py
@@ -6,6 +6,7 @@ from datetime import datetime
 import tensorboardX
 import torch
 from torch.utils.data import DataLoader
+from torchvision.transforms import RandomHorizontalFlip, RandomRotation
 from tqdm import tqdm, trange
 
 from data.Faces import Faces
@@ -16,12 +17,16 @@ from utils.EMA import EMA
 def get_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--batch_size", default=64, type=int)
+    parser.add_argument("--batch_size", default=128, type=int)
     parser.add_argument("--epochs", default=100, type=int)
 
+    parser.add_argument("--H", default=128, type=int)
+    parser.add_argument("--W", default=128, type=int)
+    
     # Dataset
-    parser.add_argument("--num_workers", default=16, type=int)
-    parser.add_argument("--train_ratio", default=0.7, type=float)
+    parser.add_argument("--lazy_load", action="store_true")
+    parser.add_argument("--num_workers", default=0, type=int)
+    parser.add_argument("--train_ratio", default=0.8, type=float)
     
     parser.add_argument("--margin", default=0.2, type=float)
     parser.add_argument("--learnable_margin", action="store_true")
@@ -34,6 +39,7 @@ def get_args():
     parser.add_argument("--tag", default="train", type=str)
     
     parser.add_argument("--test", action="store_true")
+    parser.add_argument("--device", default="cuda", type=str)
     
     args = parser.parse_args()
     return args
@@ -42,9 +48,9 @@ def get_args():
 def metric(anchor, positive, negative, threshold=0.1):
     dist_ap = torch.norm(anchor - positive, dim=-1)
     dist_an = torch.norm(anchor - negative, dim=-1)
-    tn = (dist_ap > threshold).mean().item()
-    fp = (dist_an < threshold).mean().item()
-    acc = tn + fp
+    tn = (dist_ap > threshold).float().mean().item()
+    fp = (dist_an < threshold).float().mean().item()
+    acc = tn + fp / 2
     return tn, fp, acc
 
 # @torch.jit.script
@@ -55,28 +61,41 @@ def triplet_loss(anchor, positive, negative, margin=0.2):
     return loss
 
 def train(args, basedir, model, train_dataset, valid_dataset, writer):
-    params = [model.parameters()]
+    params = [{ 'params': model.parameters() }]
     
     margin = torch.tensor(args.margin).to(args.device)
     if args.learnable_margin:
         margin.requires_grad_()
-        params.appennd(margin)
+        params.append({ 'params': margin })
     
     train_dl = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
     valid_dl = DataLoader(valid_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
     optimizer = torch.optim.Adam(params)
     
     t_ema = EMA(0.99)
+    flipper = RandomHorizontalFlip(p=1.0)
+    rotater = RandomRotation(degrees=10)
     
     for epoch in range(args.epochs):
+        tqdm.write(f"Epoch: { epoch }")
         model.train()
-        for i_batch, data in tqdm(enumerate(train_dl)):
+        for i_batch, data in enumerate(tqdm(train_dl)):
             optimizer.zero_grad()
-            faces_anc, faces_pos, faces_neg = data
+            faces_anc, faces_pos, faces_neg, idx_anc, idx_pos, idx_neg = data
             
-            ft_anc = model(faces_anc)
-            ft_pos = model(faces_pos)
-            ft_neg = model(faces_neg)
+            pos_flip = (idx_anc == idx_pos)
+            pos_flip = torch.rand(pos_flip.shape, device=args.device) < 0.5 + pos_flip
+            neg_flip = torch.rand(pos_flip.shape, device=args.device) < 0.5
+            
+            pos_flip_idx = torch.where(pos_flip)[0]
+            neg_flip_idx = torch.where(neg_flip)[0]
+            
+            faces_pos[pos_flip_idx] = flipper(faces_pos[pos_flip_idx])
+            faces_neg[neg_flip_idx] = flipper(faces_neg)[neg_flip_idx]
+            
+            ft_anc = model(rotater(faces_anc))
+            ft_pos = model(rotater(faces_pos))
+            ft_neg = model(rotater(faces_neg))
             
             loss = triplet_loss(ft_anc, ft_pos, ft_neg, margin=margin.item())
             loss.backward()
@@ -96,11 +115,13 @@ def train(args, basedir, model, train_dataset, valid_dataset, writer):
                     writer.add_scalar("train/f_pos", fp, i_batch)
                     writer.add_scalar("train/accuracy", acc, i_batch)
                     writer.add_scalar("train/triplet_loss", loss.item(), i_batch)    
+                    
+                    tqdm.write(f"\tLoss: {loss.item():.4f}, Margin: {margin:.4f}, Threshold: {threshold:.4f}, Accuracy: {acc:.4f}, tn: {tn:.4f}, fp: {fp:.4f}")
                 
         with torch.no_grad():
             model.eval()
-            for i_batch, data in tqdm(enumerate(valid_dl)):
-                faces_anc, faces_pos, faces_neg = data
+            for i_batch, data in enumerate(tqdm(valid_dl)):
+                faces_anc, faces_pos, faces_neg, idx_anc, idx_pos, idx_neg = data
                 
                 ft_anc = model(faces_anc)
                 ft_pos = model(faces_pos)
@@ -130,7 +151,7 @@ def test(args, basedir, model, test_ds, writer):
     test_dl = DataLoader(test_ds, batch_size=args.batch_size, drop_last=False, shuffle=False, num_workers=args.num_workers)
     
     manifold_dist, recog_res = [], []
-    for i_batch, data in tqdm(enumerate(test_dl)):
+    for i_batch, data in enumerate(tqdm(test_dl)):
         img_0, img_1 = data
         ft_0 = model(img_0)
         ft_1 = model(img_1)
@@ -160,18 +181,20 @@ def load_ckpt(path, model, optimizer=None):
 if __name__ == '__main__':
     args = get_args()
     
-    basedir = datetime.now().strftime("%Y-%m-%d/%H-%M-%S") + f"_{ args.tag }"
+    basedir = "results/" + datetime.now().strftime("%Y-%m-%d/%H-%M-%S") + f"_{ args.tag }"
     os.makedirs(basedir, exist_ok=True)
     
     writer = tensorboardX.SummaryWriter(log_dir=basedir)
-    recognet = RecogNet(128, 128, len_embedding=256, backbone=args.backbone)
+    recognet = RecogNet(args.H, args.W, len_embedding=256, backbone=args.backbone).to(args.device)
     
     if not args.test:
-        train_ds = Faces("data", args.batch_size, mode='train', lazy=False, preload_device='cuda', device='cuda')
-        valid_ds = Faces("data", args.batch_size, mode='valid', lazy=False, preload_device='cuda', device='cuda')
+        with torch.no_grad():
+            train_ds = Faces("data", args.batch_size, args.H, args.W, mode='train', train_ratio=args.train_ratio, lazy=args.lazy_load, preload_device='cuda', device='cuda')
+            valid_ds = Faces("data", args.batch_size, args.H, args.W, mode='valid', train_ratio=args.train_ratio, lazy=args.lazy_load, preload_device='cuda', device='cuda')
         train(args, basedir, recognet, train_ds, valid_ds, writer)
 
     else:
-        test_ds = Faces("data", args.batch_size, mode='test', lazy=False, preload_device='cuda', device='cuda')
+        with torch.no_grad():
+            test_ds = Faces("data", args.batch_size, mode='test', lazy=args.lazy_load, preload_device='cuda', device='cuda')
         test(args, basedir, recognet, test_ds, writer)
         
